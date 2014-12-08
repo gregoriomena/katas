589729a3239ad31c5d7aa82e255cc96d9ca7a882
gregoriomena <gregoriomena@gmail.com>
2014-12-08 12:49:26 +0000

    Caso 01: 2*2 = 2+2 -> VERDE

--- test/es/katas/potencia/CaluladoraDePotenciasTest.java
+++ test/es/katas/potencia/CaluladoraDePotenciasTest.java
@@ -1,5 +1,29 @@
 package es.katas.potencia;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class CaluladoraDePotenciasTest {
 
+	/*
+	 * TODO
+	 * - 2^3 = 2*2*2
+	 * - 2*2 = 2+2			<-
+	 * - 0*2 = 0			
+	 * - 2*0 = 0			
+	 * - 4*3 = 4+4+4
+	 * - 2^3 = 2*2*2
+	 * - 3^4 = 3*3*3*3
+	 */
+	
+	@Test public void
+	multiplicar_es_sumar_el_multiplicando_a_si_mismo_tantas_veces_diga_multiplicador() {
+		assertEquals(4, multiplicar(2, 2));
+	}
+
+	private int multiplicar(int i, int j) {
+		return 2+2;
+	}
 }
+
